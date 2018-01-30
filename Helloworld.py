bda0d432a73d281a72b3e503150be86543e2fb52
GentleNinja <34816558+GentleNinja@users.noreply.github.com>
2018-01-30 10:47:55 -0600

    testing github

---
+++ Helloworld.py
@@ -0,0 +1,9 @@
+#Helloworld submit codetest for github
+print('hello world')
+#end helloworld
+print("double quotes work as well")
+#try to stick with one over the other don't go back and forth.
+print("""triple quotes will allow you to print out multiple lines without using the feature slash n to get a new line""")
+print('you can also insert seperate strings' + "with a plus sign")
+#inserting quotes into text within the string with the special character \
+print('you can also insert a quote printed into the string with a \"these items will appear in quotes"')
