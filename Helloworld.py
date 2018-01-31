4c6c3f508c0bf7d92b3421991bf24549763aef8d
GentleNinja <34816558+GentleNinja@users.noreply.github.com>
2018-01-31 11:19:53 -0600

    further string manipulation practice

--- Helloworld.py
+++ Helloworld.py
@@ -7,3 +7,10 @@ print("""triple quotes will allow you to print out multiple lines without using 
 print('you can also insert seperate strings' + "with a plus sign")
 #inserting quotes into text within the string with the special character \
 print('you can also insert a quote printed into the string with a \"these items will appear in quotes"')
+#what if you want to print \n?
+print('Insert two slashes to get the slash to appear before an n \\news')
+#now we're introducing user input
+name = input('What is your name? ')
+#also adding a variable
+#now let's see how it stored the info.
+print(name)
